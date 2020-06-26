849b4db31c39656bd54fa13f79ad9b04519e9eb5
Dania Duany <deduany@gmail.com>
2020-06-26 07:51:57 +0200

    moviesMenu Style list and list items Add year
    Reported-by: Dania Duany

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles/global.scss";
 import { Header } from "./components/Header/Header";
 import { SearchInput } from "./components/SearchInput/SearchInput";
 import { getMovies } from "./components/api/api";
-import {MoviesMenu} from "components/MoviesMenu/MoviesMenu";
+import { MoviesMenu } from "components/MoviesMenu/MoviesMenu";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -17,13 +17,6 @@ function App() {
     <div className="App">
       <Header />
       <SearchInput showMovies={showMovies} />
-      {/*{movies.map((movie) => {*/}
-      {/*  return (*/}
-      {/*    <div>*/}
-      {/*      {movie.title} <img src={movie.poster} alt={movie.title} />*/}
-      {/*    </div>*/}
-      {/*  );*/}
-      {/*})}*/}
       <MoviesMenu movies={movies} />
     </div>
   );
--- src/components/MoviesMenu/MoviesMenu.js
+++ src/components/MoviesMenu/MoviesMenu.js
@@ -1,22 +1,26 @@
 import React from "react";
 import styles from "./MoviesMenu.module.scss";
 
-export function MoviesMenu({movies}){
+export function MoviesMenu({ movies }) {
 
-    return(
-        <div>
-            <span>Sharing a few of our favorite movies</span>
-            <div>
-                <ul>
-                    {movies.map(({title, poster}) => {
-                        return (
-                            <li>
-                                {title} <img src={poster} alt={title} />
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.subtitleAndMoviesLayout}>
+      <span className={styles.subtitle}>
+        Sharing a few of our favorite movies
+      </span>
+      <div className={styles.moviesLayout}>
+        <ul className={styles.moviesList}>
+          {movies.map(({ title, poster, year }) => {
+            return (
+              <li className={styles.movieItem}>
+                <span className={styles.movieTitle}>{title} </span>{" "}
+                <img className={styles.moviePic} src={poster} alt={title} />
+                <span>{year}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
