522ac1b76ab05ca1c4727c06b3e057f94a3fab80
Dania Duany <deduany@gmail.com>
2020-06-30 14:46:22 +0200

    moviesMenu
    	Resolve PR changes
    
    Reported-by: Dania Duany

--- src/components/MoviesMenu/Movies/Movies.js
+++ src/components/MoviesMenu/Movies/Movies.js
@@ -1,22 +1,20 @@
 import React from "react";
 import styles from "./Movies.module.scss";
 
-
-export function Movies({movies}){
-
-
-    return(<div className={styles.moviesLayout}>
-        <ul className={styles.moviesList}>
-            {movies.map(({ title, poster, year }) => {
-                return (
-                    <li className={styles.movieItem}>
-                        <span className={styles.movieTitle}>{title} </span>{" "}
-                        <img className={styles.moviePic} src={poster} alt={title} />
-                        <span>{year}</span>
-                    </li>
-                );
-            })}
-        </ul>
-    </div>)
-
+export function Movies({ movies }) {
+  return (
+    <div className={styles.moviesLayout}>
+      <ul className={styles.moviesList}>
+        {movies.map(({ title, poster, year }) => {
+          return (
+            <li className={styles.movieItem}>
+              <span className={styles.movieTitle}>{title} </span>
+              <img className={styles.moviePic} src={poster} alt={title} />
+              <span>{year}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
