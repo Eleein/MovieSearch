ae38aa05b59a9b1ae5c62d97a1fec1a90e6d2c9d
Dania Duany <deduany@gmail.com>
2020-06-30 17:44:17 +0200

    moviesMenu
    	Add PR changes
    
    Reported-by: Dania Duany

--- src/components/MoviesMenu/Movies/Movies.js
+++ src/components/MoviesMenu/Movies/Movies.js
@@ -7,10 +7,10 @@ export function Movies({ movies }) {
       <ul className={styles.moviesList}>
         {movies.map(({ title, poster, year }) => {
           return (
-            <li className={styles.movieItem}>
-              <span className={styles.movieTitle}>{title} </span>
+            <li className={styles.movieItem} title={title}>
+                <span  className={styles.movieTitle}>{title}</span>
               <img className={styles.moviePic} src={poster} alt={title} />
-              <span>{year}</span>
+              <span>({year})</span>
             </li>
           );
         })}
